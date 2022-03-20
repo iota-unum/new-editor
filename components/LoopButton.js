02fe7321080d46ba0149ba8d22723014ffa769a2
luigi <luigilamastra@gmail.com>
2022-03-20 20:23:44 +0100

    added persistent state with Zustand

---
+++ components/LoopButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoopButton() {
+  return (
+    <div>LoopButton</div>
+  )
+}
+
+export default LoopButton
--- store.js
+++ store.js
@@ -1,27 +1,28 @@
 import create from 'zustand';
-
-const useStore = create((set) => ({
-html: '<div></div>',
-setHtml: newHtml => set({html: newHtml}),
-containerWidth: '100%',
-height: 0,
-preview: false,
-overflow: false,
-fontSize: 1,
-selectedColor:  'rgb(0, 186, 124)',
-fontColor: 'white',
-commandState: {bold: false, italic: false, heading: false, text: true, left: true, center: false},
-setWidth: width => set({containerWidth: width}),
-togglePreview:()=> set(state => ({preview: !state.preview})),
-setPreviewToFalse: ()=> set({preview:false}),
-setCommandState: (commandState) => set(state => ({...state.commandState, commandState})),
-setOverflow: (overflow)=>set({overflow}),
-setFontSize: (fontSize) => set(fontSize),
-setSelectedColor: color => set({selectedColor: color}),
-setFontColor: color => set({fontColor: color}),
-increaseFontsize: ()=> set(state => ({fontSize: state.fontSize + 0.1})),
-decreaseFontsize: ()=> set(state => ({fontSize: state.fontSize > 0.75 ?  state.fontSize - 0.1 : 0.75}))
-
-}));
+import {persist, devtools} from 'zustand/middleware'
+const store = (set) => ({
+    html: '<div></div>',
+    setHtml: newHtml => set({html: newHtml}),
+    containerWidth: '100%',
+    height: 0,
+    preview: false,
+    overflow: false,
+    fontSize: 1,
+    selectedColor:  'rgb(0, 186, 124)',
+    fontColor: 'white',
+    commandState: {bold: false, italic: false, heading: false, text: true, left: true, center: false},
+    setWidth: width => set({containerWidth: width}),
+    togglePreview:()=> set(state => ({preview: !state.preview})),
+    setPreviewToFalse: ()=> set({preview:false}),
+    setCommandState: (commandState) => set(state => ({...state.commandState, commandState})),
+    setOverflow: (overflow)=>set({overflow}),
+    setFontSize: (fontSize) => set(fontSize),
+    setSelectedColor: color => set({selectedColor: color}),
+    setFontColor: color => set({fontColor: color}),
+    increaseFontsize: ()=> set(state => ({fontSize: state.fontSize + 0.1})),
+    decreaseFontsize: ()=> set(state => ({fontSize: state.fontSize > 0.75 ?  state.fontSize - 0.1 : 0.75}))
+    
+    })
+const useStore = create(devtools(persist(store)));
 
 export default useStore
