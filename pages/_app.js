d65e8dfd93924ba796ac62ac1533d655d0e4c41c
luigi <luigilamastra@gmail.com>
2022-03-21 14:15:42 +0100

    user/scalable no

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
 
     <Layout>
+      <Head>
+        <meta name="viewport user-scalable=no" />
+      </Head>
 
       <Component {...pageProps} />
 
