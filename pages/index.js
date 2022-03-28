45a88481973559aba90d549087ffe71e23211e2a
luigi <luigilamastra@gmail.com>
2022-03-28 20:04:28 +0200

    typo

--- pages/index.js
+++ pages/index.js
@@ -98,7 +98,7 @@ export default function Home() {
             justify-content: center;
             letter-spacing: -1px;
           }
-          .thin 
+          .thin {
             font-weight: 100;
           }
 
