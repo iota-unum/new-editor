2f1d6ae49153790efe01cb3dca9e4b6b039a12b7
luigi <luigilamastra@gmail.com>
2022-04-16 22:06:39 +0200

    padding editor  1rem

--- components/Layout.js
+++ components/Layout.js
@@ -51,7 +51,7 @@ console.log('EFFECT')
             : `calc(var(--containerWidth) * 0.5625)`};
           background-color: var(--selectedColor);
           overflow: ${preview ? 'hidden' : 'auto'};
-          padding: 0.8rem;
+          padding: 1em;
           color: ${fontColor};
           overflow-wrap: break-word;
         }
